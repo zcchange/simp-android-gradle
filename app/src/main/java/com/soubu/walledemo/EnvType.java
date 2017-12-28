e5c6319a09602f35b5168490d7356b80d8246444
zhengcheng <zhengcheng@cmhi.chinamobile.com>
2017-12-28 21:17:36 +0800

    diyici

---
+++ app/src/main/java/com/soubu/walledemo/EnvType.java
@@ -0,0 +1,8 @@
+package com.soubu.walledemo;
+
+
+public class EnvType {
+    public static final int DEVELOP = 1;//开发环境
+    public static final int CHECK = 2;//测试环境
+    public static final int PRODUCT = 3;//正式环境
+}
